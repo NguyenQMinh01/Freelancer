16d6251137d457f77eaa81cf618ef66d1345ac53
ZeroToH3ro <mtblaser2002@gmail.com>
2022-10-25 09:48:11 +0700

    minh trinh

--- db/migrate/20221014024229_devise_create_users.rb
+++ db/migrate/20221014024229_devise_create_users.rb
@@ -13,6 +13,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
 
       ## Rememberable
       t.datetime :remember_created_at
+      t.timestamps
 
       ## Trackable
       # t.integer  :sign_in_count, default: 0, null: false
