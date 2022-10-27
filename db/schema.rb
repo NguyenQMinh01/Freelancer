437a7f5cfb5ca7d8c9728a36b9d0c226c068701f
NguyenQMinh01 <minhmon112233@gmail.com>
2022-10-27 15:41:46 +0700

    model controller requests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_25_111807) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_27_082218) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -101,6 +101,19 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_25_111807) do
     t.index ["gig_id"], name: "index_pricings_on_gig_id"
   end
 
+  create_table "requests", force: :cascade do |t|
+    t.text "description"
+    t.string "title"
+    t.integer "budget"
+    t.integer "delivery"
+    t.bigint "user_id", null: false
+    t.bigint "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_requests_on_category_id"
+    t.index ["user_id"], name: "index_requests_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -129,4 +142,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_25_111807) do
   add_foreign_key "orders", "users", column: "buyer_id"
   add_foreign_key "orders", "users", column: "seller_id"
   add_foreign_key "pricings", "gigs"
+  add_foreign_key "requests", "categories"
+  add_foreign_key "requests", "users"
 end
