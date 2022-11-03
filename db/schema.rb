3f48fec4f7b73e361897afbb39d3aec0ab6e9c08
TaDuc <vtduczxt@gmail.com>
2022-11-03 15:42:16 +0700

    merge from taduc

--- db/schema.rb
+++ db/schema.rb
@@ -137,6 +137,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_02_105226) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.string "full_name"
     t.string "from"
     t.text "about"
@@ -145,10 +149,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_02_105226) do
     t.string "provider"
     t.string "uid"
     t.string "image"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.datetime "unconfirmed_email"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
