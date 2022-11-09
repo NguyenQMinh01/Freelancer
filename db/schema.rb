f06810ac86c6d455cc7a3b59d2a8329bfff37d51
TaDuc <vtduczxt@gmail.com>
2022-11-09 11:36:07 +0700

    design navbar and footer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_04_071227) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_03_102052) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -118,7 +118,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_04_071227) do
     t.integer "status", default: 0
     t.string "seller_name"
     t.string "buyer_name"
-    t.bigint "gig_id"
+    t.bigint "gig_id", null: false
     t.bigint "buyer_id"
     t.bigint "seller_id"
     t.datetime "created_at", null: false
@@ -163,6 +163,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_04_071227) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.string "full_name"
     t.string "from"
     t.text "about"
@@ -171,10 +175,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_04_071227) do
     t.string "provider"
     t.string "uid"
     t.string "image"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.datetime "unconfirmed_email"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
