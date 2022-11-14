3d4a2c77cb2be68ee4a97dfaf3fc2d16cb2f1be2
NguyenQMinh01 <minhmon112233@gmail.com>
2022-11-14 17:25:18 +0700

    fix reset password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
     skip_before_action :verify_authenticity_token
     #protect_from_forgery with: :null_session
-    before_action :set_i18n_locale
+    # before_action :set_i18n_locale
     before_action :configure_permitted_parameters, if: :devise_controller?
   
     protected
@@ -15,19 +15,19 @@ class ApplicationController < ActionController::Base
       dashboard_path
     end
     
-    def set_i18n_locale 
-      if params[:locale] 
-          if I18n.available_locales.include?(params[:locale].to_sym)
-              I18n.locale = params[:locale] 
-          else
-              flash.now[:notice] = params[:locale] + ' is not supported'              
-          end
-      end
-  end
+  #   def set_i18n_locale 
+  #     if params[:locale] 
+  #         if I18n.available_locales.include?(params[:locale].to_sym)
+  #             I18n.locale = params[:locale] 
+  #         else
+  #             flash.now[:notice] = params[:locale] + ' is not supported'              
+  #         end
+  #     end
+  # end
 
-  def default_url_options 
-      { :locale => I18n.locale }
-  end
+  # def default_url_options 
+  #     { :locale => I18n.locale }
+  # end
     
 
    
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,12 +74,15 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
   config.action_mailer.default_url_options = { host: '127.0.0.1', port: 3000 }
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :user_name            => "vtduczxt@gmail.com",
-    :password             => "beuzdncfcyocbftq",
+    :domain               => 'gmail.com',
+    :user_name            => "minhmon112233@gmail.com",
+    :password             => "adrnrlljchymazla",
     :authentication       => "plain",
     :enable_starttls_auto => true
   }
