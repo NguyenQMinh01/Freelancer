7a0d9e237f44200636e7f2d0662053eaa98bafc8
TaDuc <vtduczxt@gmail.com>
2022-11-18 14:56:31 +0700

    d

--- db/schema.rb
+++ db/schema.rb
@@ -147,7 +147,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_18_031847) do
     t.integer "status", default: 0
     t.string "seller_name"
     t.string "buyer_name"
-    t.bigint "gig_id"
+    t.bigint "gig_id", null: false
     t.bigint "buyer_id"
     t.bigint "seller_id"
     t.datetime "created_at", null: false
@@ -207,6 +207,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_18_031847) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.string "full_name"
     t.string "from"
     t.text "about"
@@ -215,10 +219,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_18_031847) do
     t.string "provider"
     t.string "uid"
     t.string "image"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.datetime "unconfirmed_email"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
